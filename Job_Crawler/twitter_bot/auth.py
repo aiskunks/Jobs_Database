c9ecadb708ed9a1aef03a8dfaa9f18e2e5960826
AI Skunks <73204069+aiskunks@users.noreply.github.com>
2022-10-06 00:05:11 -0400

    Add files via upload

--- Job_Crawler/twitter_bot/auth.py
+++ Job_Crawler/twitter_bot/auth.py
@@ -1,14 +1,15 @@
 
 class TwitterAuth:
+	# You need to go to http://dev.twitter.com to get your own credentials 
 	# Go to http://dev.twitter.com and create an app. 
 	# The consumer key and secret will be generated for you after
-	consumer_key="KxOJm72T5TNntTogDmHySbIiA"  #API key
-	consumer_secret="zLZ7XcxB9IGKqrvntPpRkm9ftn4qrfX67DHfi4z1wJQAktIwNL"  # API Key Secret
+	consumer_key="KxOJm72TtTogDmHySbIiA"  #API key
+	consumer_secret="zLZ7XcxB9m9ftn4qrfX67DHfi4z1wJQAktIwNL"  # API Key Secret
 
 	# After the step above, you will be redirected to your app's page.
 	# Create an access token under the the "Your access token" section
-	access_token="34749634-b7MblPYKBRXFy8JKCRkhjz27U5shHd5ocN5Xq4R0P"
-	access_token_secret="9H57Pa3RjoCoi5TjlTpJVuBhuugDItmg0MyW8jHPRFiG9"
+	access_token="34749634-b7MblPYKBRjz27U5shHd5ocN5Xq4R0P"
+	access_token_secret="9H57Pa3RjouBhuugDItmg0MyW8jHPRFiG9"
 	
 	
 	
