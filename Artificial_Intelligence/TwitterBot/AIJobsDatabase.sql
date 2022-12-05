471f6d4cd1f537acc242e602c15f11c32bf13e63
Hariharan Sundaram <gautham.apa@gmail.com>
2022-12-05 16:14:47 -0500

    Updated sql queries to create jobs data tables.

--- Artificial_Intelligence/TwitterBot/AIJobsDatabase.sql
+++ Artificial_Intelligence/TwitterBot/AIJobsDatabase.sql
@@ -7,6 +7,10 @@ USE `ai_jobs_database`;
 SET NAMES utf8mb4;
 SET character_set_client = utf8mb4;
 
+/*
+Tweet Data Tables
+*/
+
 CREATE TABLE `twitter_user` (
 `user_id` bigint NOT NULL,
 `name` varchar(50) NOT NULL,
@@ -56,3 +60,61 @@ CREATE TABLE `derived_tag` (
   `tweet_id` BIGINT NOT NULL,
   PRIMARY KEY (`der_tag_name`,`tweet_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+/*
+Job Data Tables
+*/
+
+CREATE TABLE `company` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `founded_year` int,
+  `sector` varchar(50),
+  `size` varchar(50),
+  `revenue` varchar(50),
+  `rating` float,
+   PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `state_data` (
+  `state` varchar(50) NOT NULL,
+  `tax_rate` float,
+  PRIMARY KEY(`state`)
+);
+
+CREATE TABLE `location` (
+  `id` bigint NOT NULL UNIQUE AUTO_INCREMENT,
+  `city` varchar(50),
+  `state` varchar(50),
+  `country` varchar(50),
+  `cost_of_living_index` float,
+  PRIMARY KEY(`city`, `state`),
+  FOREIGN KEY(`state`) REFERENCES state_data(`state`)
+);
+
+CREATE TABLE `jobData` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `title` varchar(50),
+  `description` varchar(500),
+  `datePosted` date,
+  `location_id` bigint,
+  `link` varchar(100) UNIQUE,
+  `seniorityLevel` varchar(50),
+  `employmentType` varchar(50),
+  `company_id` bigint,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`company_id`) REFERENCES company(`id`),
+  FOREIGN KEY (`location_id`) REFERENCES location(`id`)
+);
+
+CREATE TABLE `derived_data` (
+  `job_id` bigint,
+  `category` varchar(50),
+  `value` varchar(50),
+  FOREIGN KEY(`job_id`) REFERENCES jobData(`id`),
+  PRIMARY KEY(`job_id`, `category`, `value`)
+);
+
+
+
